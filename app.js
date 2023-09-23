4e305a5fea387f66ca09601f1cf209d8199e1dcf
rich <dankwarrior6900@gmail.com>
2023-09-22 19:19:30 -0700

    Make the layout

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 "use strict"
+
+let firstNumber;
+let secondNumber;
+let operand;
+
+function addition(a, b) {
+    return a + b;
+};
+
+function subtraction(a, b) {
+    return a - b;
+};
+
+function multiplication(a, b) {
+    return a * b;
+};
+
+function division(a, b) {
+    return a / b;
+};
+
+function operate(a, b, operation) {
+    if (operation === '+') {
+        addition(a, b);
+    } else if (operation === '-') {
+        subtraction(a, b);
+    } else if (operation === 'x') {
+        multiplication(a, b);
+    } else {
+        division(a, b);
+    };
+};

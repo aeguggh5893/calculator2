2b362dc90da38f465a535f01fcf3264afb7ac38c
rich <dankwarrior6900@gmail.com>
2023-09-24 14:26:19 -0700

    This was what I was able to do from memory

--- app.js
+++ app.js
@@ -4,119 +4,108 @@ let firstValue = "";
 let secondValue = "";
 let operand = "";
 
-function addition(a, b) {
-    return parseFloat(a) + parseFloat(b);
-};
 
-function subtraction(a, b) {
-    return parseFloat(a) - parseFloat(b);
-};
 
-function multiplication(a, b) {
-    return parseFloat(a) * parseFloat(b);
-};
-
-function division(a, b) {
-    if(b == 0) {alert("Can't divide by 0!");
-    return;
-    };  
-
-    return parseFloat(a) / parseFloat(b);
-};
-
-function operate(a, b, operation) {
-    if (operation === '+') {
-        return addition(a, b);
-    } else if (operation === '-') {
-        return subtraction(a, b);
-    } else if (operation === 'x') {
-       return multiplication(a, b);
+function operate(num1, num2, op) {
+    if (op === "+") {
+        return parseFloat(num1) + parseFloat(num2);
+    } else if (op === "-") {
+        return parseFloat(num1) - parseFloat(num2);
+    } else if (op === "x") {
+        return parseFloat(num1) * parseFloat(num2);
     } else {
-       return division(a, b);
+        return parseFloat(num1) / parseFloat(num2);
     };
 };
 
-let numberButtons = document.querySelectorAll('.number');
-let operationButtons = document.querySelectorAll('.operation');
-let currentDisplay = document.querySelector('#current');
 let previousDisplay = document.querySelector('#previous');
+let currentDisplay = document.querySelector('#current');
+
 let clearButton = document.querySelector('.clear');
-let deleteButton = document.querySelector('.delete');
-let decimalButton = document.querySelector('#decimal');
-
-
-numberButtons.forEach(item => {
-    item.addEventListener('click', e => {
-        if (operand === "") {
-            firstValue += e.target.innerText;
-            currentDisplay.innerText += e.target.innerText;
-        } else {
-            secondValue += e.target.innerText;
-            currentDisplay.innerText += e.target.innerText
-        };
-    });
-});
-
-
-operationButtons.forEach(item => {
-    item.addEventListener('click', e => {
-       
-        if (firstValue && secondValue && e.target.innerText !== '=') {
-            let answer = operate(firstValue, secondValue, operand);
-            firstValue = answer.toString();
-            currentDisplay.innerText = firstValue;
-            previousDisplay.innerText = "";
-            operand = e.target.innerText;
-            secondValue = "";
-        };
-        
-        if (e.target.innerText !== "=") {
-            operand = e.target.innerText;
-            currentDisplay.innerText += ` ${e.target.innerText}`;
-            previousDisplay.innerText = currentDisplay.innerText;
-            currentDisplay.innerText = "";
-        } else {
-            let answer = operate(firstValue, secondValue, operand);
-            firstValue = answer.toString();
-            currentDisplay.innerText = firstValue;
-            previousDisplay.innerText = "";
-            operand = "";
-            secondValue = "";
-        };
-    });
-});
-
 clearButton.addEventListener('click', function() {
-    currentDisplay.innerText = "";
+    currentDisplay.innerText = "0";
     previousDisplay.innerText = "";
     firstValue = "";
     secondValue = "";
     operand = "";
 });
 
+
+let deleteButton = document.querySelector('.delete');
 deleteButton.addEventListener('click', function() {
-    if (secondValue.length > 0) {
-        let string = secondValue.split('');
-        string.pop();
-        secondValue = string.join('');
-        currentDisplay.innerText = secondValue;
-    } else {
+    if (firstValue.length > 0 && currentDisplay.innerText === firstValue) {
         let string = firstValue.split('');
         string.pop();
         firstValue = string.join('');
         currentDisplay.innerText = firstValue;
+    } else {
+        let string = secondValue.split('');
+        string.pop();
+        secondValue = string.join('');
+        currentDisplay.innerText = secondValue;
     };
 });
 
+
+let decimalButton = document.querySelector('.decimal');
 decimalButton.addEventListener('click', function(e) {
-    if (!firstValue.includes('.')) {
+    if (!secondValue.includes('.')) {
+        secondValue += e.target.innerText;
+        currentDisplay.innerText = secondValue;
+    } else {
         firstValue += e.target.innerText;
         currentDisplay.innerText = firstValue;
-    } else {
-        if (!secondValue.includes('.')) {
-            secondValue += e.target.innerText;
-            currentDisplay.innerText = secondValue;
-        };
     };
 });
 
+let numberButtons = document.querySelectorAll('.number');
+let operationButtons = document.querySelectorAll('.operation');
+
+
+
+numberButtons.forEach(item => {
+    item.addEventListener('click', function(e) {
+        if (operand === "") {
+            if (currentDisplay.innerText === '0') {
+                currentDisplay.innerText = "";
+            };
+            firstValue += e.target.innerText;
+            currentDisplay.innerText += e.target.innerText;
+        } else {
+            secondValue += e.target.innerText;
+            currentDisplay.innerText += e.target.innerText;
+        };
+    });
+});
+
+operationButtons.forEach(item => {
+    item.addEventListener('click', function(e) {
+       if (operand === "" && e.target.innerText !== '=') {
+        operand = e.target.innerText;
+        currentDisplay.innerText += ` ${e.target.innerText}`;
+        previousDisplay.innerText = currentDisplay.innerText;
+        currentDisplay.innerText = "";
+        } else if (e.target.innerText === "=") {
+            if (firstValue === "" || operand === "" || secondValue === "") {
+                return;
+            } else {
+                let answer = operate(firstValue, secondValue, operand);
+                answer = answer.toString();
+                firstValue = answer;
+                operand = "";
+                secondValue = "";
+                previousDisplay.innerText = "";
+                currentDisplay.innerText = answer;
+            };
+        } else {
+            let answer = operate(firstValue, secondValue, operand);
+            answer = answer.toString();
+            firstValue = answer;
+            operand = "";
+            operand = e.target.innerText;
+            secondValue = "";
+            currentDisplay.innerText = "";
+            previousDisplay.innerText = `${answer}  ${e.target.innerText}`;
+        };
+    });
+});
